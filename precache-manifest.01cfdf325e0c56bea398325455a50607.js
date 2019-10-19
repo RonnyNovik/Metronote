508647f683464d6a3251b8a89f6f11dcc2bbf11b
RonnyNovik <novikdesigndev@gmail.com>
2019-10-19 03:12:24 +0300

    css fix

---
+++ precache-manifest.01cfdf325e0c56bea398325455a50607.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7307a896c14d3a99edf2edfc0069b10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43a11b81e3d5197f6db3",
+    "url": "./static/css/2.62f4602b.chunk.css"
+  },
+  {
+    "revision": "236dc900420d13e52a6b",
+    "url": "./static/css/main.6bfe8b41.chunk.css"
+  },
+  {
+    "revision": "43a11b81e3d5197f6db3",
+    "url": "./static/js/2.c95b3692.chunk.js"
+  },
+  {
+    "revision": "236dc900420d13e52a6b",
+    "url": "./static/js/main.b03f75ba.chunk.js"
+  },
+  {
+    "revision": "57b161ff6a92a68d715a",
+    "url": "./static/js/runtime-main.c8119b70.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
